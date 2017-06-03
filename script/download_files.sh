f57d05d40a185a64f54d588a539eea8c707c06c9
Sultan <salzahrani@gmail.com>
2017-06-03 02:27:55 -0700

    changing gitignore-all

--- script/download_files.sh
+++ script/download_files.sh
@@ -1,7 +1,7 @@
-curl -o ./conceptsgenerator/src/main/java/triplet/nlp/model/ http://www.public.asu.edu/~ssalzahr/models_jars/dep_MODEL0.mdl
-curl -o ./conceptsgenerator/src/main/java/triplet/nlp/model/ http://www.public.asu.edu/~ssalzahr/models_jars/pos_MODEL0.mdl
-curl -o ./conceptsgenerator/src/main/java/triplet/nlp/model/ http://www.public.asu.edu/~ssalzahr/models_jars/srl_MODEL0.mdl
-curl -o ./conceptsgenerator/src/main/java/triplet/nlp/model/ http://www.public.asu.edu/~ssalzahr/models_jars/srl_MODEL1.mdl
-curl -o ./conceptsgenerator/jars/CleanNLPJARS/ http://www.public.asu.edu/~ssalzahr/models_jars/stanford-corenlp-3.4-models.jar
-curl -o ./conceptsgenerator/jars/AllCombinationJAR/ http://www.public.asu.edu/~ssalzahr/models_jars/jars/AllCombinationJAR/stanford-corenlp-models-2011-12-27.jar
+curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/dep_MODEL0.mdl  http://www.public.asu.edu/~ssalzahr/models_jars/dep_MODEL0.mdl
+curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/pos_MODEL0.mdl  http://www.public.asu.edu/~ssalzahr/models_jars/pos_MODEL0.mdl
+curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/srl_MODEL0.mdl http://www.public.asu.edu/~ssalzahr/models_jars/srl_MODEL0.mdl
+curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/srl_MODEL1.mdl http://www.public.asu.edu/~ssalzahr/models_jars/srl_MODEL1.mdl
+curl -o  conceptsgenerator_0/jars/CleanNLPJARS/stanford-corenlp-3.4-models.jar http://www.public.asu.edu/~ssalzahr/models_jars/stanford-corenlp-3.4-models.jar
+curl -o  conceptsgenerator_0/jars/AllCombinationJAR/stanford-corenlp-models-2011-12-27.jar  http://www.public.asu.edu/~ssalzahr/models_jars/jars/AllCombinationJAR/stanford-corenlp-models-2011-12-27.jar
 
