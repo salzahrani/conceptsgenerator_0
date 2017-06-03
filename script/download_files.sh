9bff21b321a9c0d333788b2c0ce5e0a55911e2ad
Sultan <salzahrani@gmail.com>
2017-06-03 04:59:07 -0700

    changing script for download

--- script/download_files.sh
+++ script/download_files.sh
@@ -1,7 +1,7 @@
-curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/dep_MODEL0.mdl  http://www.public.asu.edu/~ssalzahr/models_jars/dep_MODEL0.mdl
-curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/pos_MODEL0.mdl  http://www.public.asu.edu/~ssalzahr/models_jars/pos_MODEL0.mdl
-curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/srl_MODEL0.mdl http://www.public.asu.edu/~ssalzahr/models_jars/srl_MODEL0.mdl
-curl -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/srl_MODEL1.mdl http://www.public.asu.edu/~ssalzahr/models_jars/srl_MODEL1.mdl
-curl -o  conceptsgenerator_0/jars/CleanNLPJARS/stanford-corenlp-3.4-models.jar http://www.public.asu.edu/~ssalzahr/models_jars/stanford-corenlp-3.4-models.jar
-curl -o  conceptsgenerator_0/jars/AllCombinationJAR/stanford-corenlp-models-2011-12-27.jar  http://www.public.asu.edu/~ssalzahr/models_jars/jars/AllCombinationJAR/stanford-corenlp-models-2011-12-27.jar
+curl -L -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/dep_MODEL0.mdl  https://www.dropbox.com/s/v65b5d6osoy9zwc/dep_MODEL0.mdl?dl=1
+curl -L -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/pos_MODEL0.mdl  https://www.dropbox.com/s/p6vcwlw2sgfkme2/pos_MODEL0.mdl?dl=1
+curl -L -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/srl_MODEL0.mdl https://www.dropbox.com/s/4infxk2pbztjnbu/srl_MODEL0.mdl?dl=1
+curl -L -o  conceptsgenerator_0/src/main/java/triplet/nlp/model/srl_MODEL1.mdl https://www.dropbox.com/s/8w4id2u8ww6tb1l/srl_MODEL1.mdl?dl=1
+curl -L -o  conceptsgenerator_0/jars/CleanNLPJARS/stanford-corenlp-3.4-models.jar https://www.dropbox.com/s/thsm52akoin3lfk/stanford-corenlp-3.4-models.jar?dl=1
+curl -L -o  conceptsgenerator_0/jars/AllCombinationJAR/stanford-corenlp-models-2011-12-27.jar  https://www.dropbox.com/s/9kbg9oh36xynkzy/stanford-corenlp-models-2011-12-27.jar?dl=1
 
